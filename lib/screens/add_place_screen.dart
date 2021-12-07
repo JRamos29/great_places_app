0c9f771b284df92916a00b82ffbb1cd31292678f
JRamos29 <malebolge@gmail.com>
2021-12-07 17:56:54 -0300

    Managing Data & Images via the Provider Package.

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -11,6 +15,20 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +38,13 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
+        children: <Widget>[
           Expanded(
             child: SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10),
                 child: Column(
-                  children: [
+                  children: <Widget>[
                     TextField(
                       decoration: InputDecoration(labelText: 'Title'),
                       controller: _titleController,
@@ -34,14 +52,14 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     SizedBox(
                       height: 10,
                     ),
-                    ImageInput()
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
             ),
           ),
           ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: _savePlace,
             icon: Icon(Icons.add),
             label: Text('Add Place'),
             style: ElevatedButton.styleFrom(
@@ -52,14 +70,6 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                 padding:
                     EdgeInsets.only(left: 0, top: 20, right: 0, bottom: 25)),
           ),
-          // RaisedButton.icon(
-          //   icon: Icon(Icons.add),
-          //   label: Text('Add Place'),
-          //   onPressed: () {},
-          //   elevation: 0,
-          //   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          //   color: Theme.of(context).accentColor,
-          // ),
         ],
       ),
     );
